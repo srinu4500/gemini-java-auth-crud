df523c1c64119c75e2c32283263368ee064a0767
Srinu <srinu1757@gmail.com>
2026-03-10 08:01:08 +0530

    Update UI with green background and Login/Register buttons

--- ui/src/app/home/home.ts
+++ ui/src/app/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../auth';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
